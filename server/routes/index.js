c546aba0d8cc2b79f3c5bd44adf3d159de8a8564
nnlinh97 <nnlinh97it@gmail.com>
2018-11-28 21:25:31 +0700

    test nodemon

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/hello', function(req, res, next) {
-  res.render('index', { title: 'Hello' });
+  res.render('index', { title: 'Hello hello' });
 });
 
 module.exports = router;
