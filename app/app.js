8a8bf8fca145e11276e7af580d0bc1c78f54897f
nnlinh97 <nnlinh97it@gmail.com>
2018-12-05 23:30:44 +0700

    chinh lai file app.js, deploy lai

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const db = require('./config/dbConfig');
@@ -11,33 +11,17 @@ const usersRouter = require('./routes/users');
 const app = express();
 db.sequelize.sync();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+const server_port = process.env.YOUR_PORT || process.env.PORT || 4200;
+const server_host = process.env.YOUR_HOST || '0.0.0.0';
+
+const server = app.listen(server_port, server_host, function () {
+  console.log(`App listening at port ${server_port}`)
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+module.exports = server;
